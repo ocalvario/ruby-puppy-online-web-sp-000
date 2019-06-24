11736cdb61a29dbe434ad341b39834b0fddc1c1a
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-24 19:20:53 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,12 +9,19 @@ class Dog
    @@all << self
   end
 
-  def self.all
+  def self.allz
     @@all
   end
   
   def self.clear_all
   end
+  
+  def self.all
+    puts @@all.map{ |dog| dog.name }
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
 
-end
 end
